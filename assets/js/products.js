ee16624448bce6e17d10a97063fea87ca5c508b8
Aakriti8 <aakritikotwani@gmail.com>
2025-07-31 17:33:59 +0530

    Product Listing

--- assets/js/products.js
+++ assets/js/products.js
@@ -1,222 +1,72 @@
 const products = [
     {
         id: 1,
-        name: "Aryan Watch",
-        image: "assets/img/products5/product-1-1.png",
-        price: 155.00,
+        name: "Clover Themed Adjustable Finger Ring",
+        image: "assets/img/Product list/ring1.jpg",
+        price: 150.00,
         isTrending: 'y',
         isBestSelling: 'y',
-        type: 'CLASSIC',
-        category: 'Watch'
+        type: 'modern',
+        category: 'Arings'
     },
     {
         id: 2,
-        name: "Aakriti Watch",
-        image: "assets/img/products5/product-1-1.png",
-        price: 155.00,
+        name: "Contemporary Adjustable Finger Ring",
+        image: "assets/img/Product list/ring2.jpg",
+        price: 199.00,
         isTrending: 'y',
         isBestSelling: 'y',
         type: 'Modern',
-        category: 'Watch'
+        category: 'Arings'
     },
     {
         id: 3,
-        name: "Golden Line",
-        image: "assets/img/products5/product-1-2.png",
+        name: "Heart Themed Layered Necklace",
+        image: "assets/img/Product list/necklace1.jpg",
         price: 145.00,
         isTrending: 'y',
         isBestSelling: 'y',
-        type: 'CLASSIC',
-        category: 'Watch'
+        type: 'Modern',
+        category: 'Anecklace'
     },
     {
         id: 4,
-        name: "Round Dial Semi",
-        image: "assets/img/products5/product-1-3.png",
+        name: "Black Geometric Pendant",
+        image: "assets/img/Product list/necklace2.jpg",
         price: 135.00,
         isTrending: 'y',
         isBestSelling: 'y',
         type: 'Modern',
-        category: 'Watch'
+        category: 'Anecklace'
     },
     {
         id: 5,
-        name: "Black Dial Red",
-        image: "assets/img/products5/product-1-4.png",
+        name: "White Emerald Layered Necklace",
+        image: "assets/img/Product list/necklace3.jpg",
         price: 125.00,
         isTrending: 'y',
         isBestSelling: 'y',
-        type: 'CLASSIC',
-        category: 'Watch'
+        type: 'Modern',
+        category: 'Anecklace'
     },
     {
         id: 6,
-        name: "Classic Leather",
-        image: "assets/img/products5/product-1-5.png",
-        price: 185.00,
+        name: "Dreamy White Stone Pendant ",
+        image: "assets/img/Product list/necklace4.jpg",
+        price: 125.00,
         isTrending: 'y',
         isBestSelling: 'y',
-        type: 'CLASSIC',
-        category: 'Watch'
+        type: 'Modern',
+        category: 'Anecklace'
     },
     {
         id: 7,
-        name: "Silver Mesh",
-        image: "assets/img/products5/product-1-6.png",
-        price: 165.00,
+        name: "White Emerald Layered Necklace",
+        image: "assets/img/Product list/necklace5.jpg",
+        price: 125.00,
         isTrending: 'y',
         isBestSelling: 'y',
         type: 'Modern',
-        category: 'Watch'
-    },
-    {
-        id: 8,
-        name: "Rose Gold Link",
-        image: "assets/img/products5/product-1-7.png",
-        price: 195.00,
-        isTrending: 'y',
-        isBestSelling: 'y',
-        type: 'Modern',
-        category: 'Watch'
-    },
-    {
-        id: 9,
-        name: "Midnight Black",
-        image: "assets/img/products5/product-1-8.png",
-        price: 175.00,
-        isTrending: 'y',
-        isBestSelling: 'y',
-        type: 'CLASSIC',
-        category: 'Watch'
-    },
-    {
-        id: 10,
-        name: "Azure Blue",
-        image: "assets/img/products5/product-1-9.png",
-        price: 160.00,
-        isTrending: 'y',
-        isBestSelling: 'y',
-        type: 'Modern',
-        category: 'Watch'
-    },
-    {
-        id: 11,
-        name: "Petal Perfection",
-        image: "assets/img/products5/bracelet-1.png",
-        price: 155.00,
-        isTrending: 'y',
-        isBestSelling: 'y',
-        type: 'Modern',
-        category: 'Bracelet'
-    },
-    {
-        id: 12,
-        name: "Golden Line",
-        image: "assets/img/products5/bracelet-2.png",
-        price: 155.00,
-        isTrending: 'y',
-        isBestSelling: 'y',
-        type: 'CLASSIC',
-        category: 'Antitarnish'
-    },
-    {
-        id: 13,
-        name: "Cropped Golden Pendant",
-        image: "assets/img/products5/bracelet-3.png",
-        price: 155.00,
-        isTrending: 'n',
-        isBestSelling: 'y',
-        type: 'Modern',
-        category: 'ABracelet'
-    },
-    {
-        id: 14,
-        name: "Petal Perfection",
-        image: "assets/img/products5/earring-1.png",
-        price: 155.00,
-        isTrending: 'y',
-        isBestSelling: 'y',
-        type: 'Modern',
-        category: 'Earring'
-    },
-    {
-        id: 15,
-        name: "Golden Line",
-        image: "assets/img/products5/earring-2.png",
-        price: 155.00,
-        isTrending: 'y',
-        isBestSelling: 'y',
-        type: 'CLASSIC',
-        category: 'Earring'
-    },
-    {
-        id: 16,
-        name: "Cropped Golden Pendant",
-        image: "assets/img/products5/earring-3.png",
-        price: 155.00,
-        isTrending: 'n',
-        isBestSelling: 'y',
-        type: 'Modern',
-        category: 'Earring'
-    },
-    {
-        id: 17,
-        name: "Petal Perfection",
-        image: "assets/img/products5/necklace-1.png",
-        price: 155.00,
-        isTrending: 'y',
-        isBestSelling: 'y',
-        type: 'Modern',
-        category: 'Necklace'
-    },
-    {
-        id: 18,
-        name: "Golden Line",
-        image: "assets/img/products5/necklace-2.png",
-        price: 155.00,
-        isTrending: 'y',
-        isBestSelling: 'y',
-        type: 'CLASSIC',
-        category: 'Necklace'
-    },
-    {
-        id: 19,
-        name: "Cropped Golden Pendant",
-        image: "assets/img/products5/necklace-3.png",
-        price: 155.00,
-        isTrending: 'n',
-        isBestSelling: 'y',
-        type: 'Modern',
-        category: 'Necklace'
-    },
-    {
-        id: 20,
-        name: "Petal Perfection",
-        image: "assets/img/products5/ring-1.png",
-        price: 155.00,
-        isTrending: 'y',
-        isBestSelling: 'y',
-        type: 'Modern',
-        category: 'Ring'
-    },
-    {
-        id: 21,
-        name: "Golden Line",
-        image: "assets/img/products5/ring-2.png",
-        price: 155.00,
-        isTrending: 'y',
-        isBestSelling: 'y',
-        type: 'CLASSIC',
-        category: 'Ring'
-    },
-    {
-        id: 22,
-        name: "Cropped Golden Pendant",
-        image: "assets/img/products5/ring-3.png",
-        price: 155.00,
-        isTrending: 'n',
-        isBestSelling: 'y',
-        type: 'Modern',
-        category: 'Ring'
+        category: 'Anecklace'
     }
 ];
