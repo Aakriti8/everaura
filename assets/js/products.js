9e1ecc9f3af25e1506341b35966251b897d596c0
Aakriti8 <aakritikotwani@gmail.com>
2025-08-01 00:06:18 +0530

    Product Listing

--- assets/js/products.js
+++ assets/js/products.js
@@ -7,7 +7,8 @@ const products = [
         isTrending: 'y',
         isBestSelling: 'y',
         type: 'modern',
-        category: 'Arings'
+        category: 'Arings',
+        isAntiTarnish: 'y'
     },
     {
         id: 2,
@@ -17,56 +18,85 @@ const products = [
         isTrending: 'y',
         isBestSelling: 'y',
         type: 'Modern',
-        category: 'Arings'
+        category: 'Arings',
+        isAntiTarnish: 'y'
     },
     {
         id: 3,
         name: "Heart Themed Layered Necklace",
         image: "assets/img/Product list/necklace1.jpg",
-        price: 145.00,
+        price: 280.00,
         isTrending: 'y',
         isBestSelling: 'y',
         type: 'Modern',
-        category: 'Anecklace'
+        category: 'Anecklace',
+        isAntiTarnish: 'y'
     },
     {
         id: 4,
-        name: "Black Geometric Pendant",
+        name: "Black Geometric Pendant Necklace",
         image: "assets/img/Product list/necklace2.jpg",
-        price: 135.00,
+        price: 235.00,
         isTrending: 'y',
         isBestSelling: 'y',
         type: 'Modern',
-        category: 'Anecklace'
+        category: 'Anecklace',
+        isAntiTarnish: 'y'
     },
     {
         id: 5,
         name: "White Emerald Layered Necklace",
         image: "assets/img/Product list/necklace3.jpg",
-        price: 125.00,
+        price: 290.00,
         isTrending: 'y',
         isBestSelling: 'y',
         type: 'Modern',
-        category: 'Anecklace'
+        category: 'Anecklace',
+        isAntiTarnish: 'y'
     },
     {
         id: 6,
         name: "Dreamy White Stone Pendant ",
         image: "assets/img/Product list/necklace4.jpg",
-        price: 125.00,
+        price: 290.00,
         isTrending: 'y',
         isBestSelling: 'y',
         type: 'Modern',
-        category: 'Anecklace'
+        category: 'Anecklace',
+        isAntiTarnish: 'y'
     },
     {
         id: 7,
-        name: "White Emerald Layered Necklace",
+        name: "Green Emerald Layered Necklace",
         image: "assets/img/Product list/necklace5.jpg",
-        price: 125.00,
+        price: 299.00,
         isTrending: 'y',
         isBestSelling: 'y',
         type: 'Modern',
-        category: 'Anecklace'
+        category: 'Anecklace',
+        isAntiTarnish: 'y'
+    },
+    {
+        id: 8,
+        name: "Vivid Pink Charm Necklace",
+        image: "assets/img/Product list/necklace6.jpg",
+        price: 299.00,
+        isTrending: 'y',
+        isBestSelling: 'y',
+        type: 'Modern',
+        category: 'Anecklace',
+        isAntiTarnish: 'y'
+    },
+    {
+        id: 9,
+        name: "Studded Hexagonal Pendant Necklace",
+        image: "assets/img/Product list/necklace7.jpg",
+        price: 299.00,
+        isTrending: 'y',
+        isBestSelling: 'y',
+        type: 'Modern',
+        category: 'Anecklace',
+        isAntiTarnish: 'y'
     }
+
 ];
